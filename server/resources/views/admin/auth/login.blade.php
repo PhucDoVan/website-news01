1f28d474e28c31bc64b1d86732e904f94f854b2f
PhucDoVan <dvphuc.sgt@gmail.com>
2020-08-19 17:48:00 +0700

    #2 coding show/hide password

--- server/resources/views/admin/auth/login.blade.php
+++ server/resources/views/admin/auth/login.blade.php
@@ -1,6 +1,19 @@
 @extends('admin.layouts.master')
 @section('title', 'Login')
 <link rel="stylesheet" href="{{asset('css/login/style.css')}}">
+<script type="text/javascript">//<![CDATA[
+
+    window.onload=function(){
+
+        /**
+         * @author Abdo-Hamoud <abdo.host@gmail.com>
+         * https://github.com/Abdo-Hamoud/bootstrap-show-password
+         * version: 1.0
+         */
+        !function(a){a(function(){a('[data-toggle="password"]').each(function(){var b = a(this); var c = a(this).parent().find(".input-group-text"); c.css("cursor", "pointer").addClass("input-password-hide"); c.on("click", function(){if (c.hasClass("input-password-hide")){c.removeClass("input-password-hide").addClass("input-password-show"); c.find(".fa").removeClass("fa-eye").addClass("fa-eye-slash"); b.attr("type", "text")} else{c.removeClass("input-password-show").addClass("input-password-hide"); c.find(".fa").removeClass("fa-eye-slash").addClass("fa-eye"); b.attr("type", "password")}})})})}(window.jQuery);
+    }
+
+    //]]></script>
 @section('content')
     <!-- Sing in  Form -->
     <div class="container">
@@ -33,20 +46,23 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="fas fa-user"></i></span>
                             </div>
-                            <input type="text" name="username" class="form-control" placeholder="username" value="{{ old('username')  }}">
+                            <input type="text" name="username" class="form-control" placeholder="username" value="{{ old('username')  }}"/>
 
                         </div>
                         <div class="input-group form-group">
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="fas fa-key"></i></span>
                             </div>
-                            <input type="password" name="password" class="form-control" placeholder="password" value="{{ old('password') }}">
+                            <input type="password" name="password" id="password" class="form-control" data-toggle="password" placeholder="password" value="{{ old('password') }}"/>
+                            <div class="input-group-append">
+                                <div class="input-group-text"><i class="fa fa-eye"></i></div>
+                            </div>
                         </div>
                         <div class="row align-items-center remember">
-                            <input type="checkbox" name="remember" id="remember_me" value="{{ old('remember') === 'on' ? 'checked' : '' }}">Remember Me
+                            <input type="checkbox" name="remember" id="remember_me" value="{{ old('remember') === 'on' ? 'checked' : '' }}"/>Remember Me
                         </div>
                         <div class="form-group">
-                            <input type="submit" name="signin" value="Login" class="btn float-right login_btn">
+                            <input type="submit" name="signin" value="Login" class="btn float-right login_btn"/>
                         </div>
                     </form>
                 </div>
